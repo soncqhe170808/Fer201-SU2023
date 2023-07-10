cff137cc8b978a8cb56765502559ee7e38d631b4
Chu Thang <thangcvhe163001@fpt.edu.vn>
2023-07-10 17:20:17 +0700

    jobdetials

--- nhom2/src/Screen/JobData.js
+++ nhom2/src/Screen/JobData.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
 import '../style/Jobdata.css';
 import "bootstrap/dist/css/bootstrap.min.css";
 import DefaultTemplate from '../Template/DefaultTemplate';
@@ -72,61 +73,65 @@ const JobData = () => {
   return (
     <DefaultTemplate>
       <div className="container">
-      <div className="row">
-        <div className="col-12">
-          <h2 className="text-center">Jobs List</h2>
-        </div>
-        <div className="col-12">
-          <div className="sort-buttons">
-            <button className="btn btn-primary" onClick={sortJobsByNewest}>
-              New
-            </button>
-            <button className="btn btn-primary" onClick={sortJobsByOldest}>
-              Old
-            </button>
+        <div className="row">
+          <div className="col-12">
+            <h2 className="text-center">Jobs List</h2>
           </div>
-        </div>
+          <div className="col-12">
+            <div className="sort-buttons">
+              <button className="btn btn-primary" onClick={sortJobsByNewest}>
+                New
+              </button>
+              <button className="btn btn-primary" onClick={sortJobsByOldest}>
+                Old
+              </button>
+            </div>
+          </div>
 
-        <div className="col-9">
-          <div className="job-data">
-            {filteredJobs.map((job) => (
-              <div key={job.id} className="job-card">
-                <img src={job.image} alt="Job" className="job-image" />
-                <h2>{job.JobName}</h2>
-                <p
-                  className={`job-description ${expandedJobId === job.id ? 'expanded' : ''}`}
-                  onClick={() => toggleJobDescription(job.id)}
-                >
-                  {expandedJobId === job.id
-                    ? job.JobDescription
-                    : job.JobDescription.substring(0, 100) + '...'}
-                </p>
-                <p>Recruitment Goal: {job.RecuitmentGoal}</p>
-                <p>Posted on: {job.PostDate}</p>
-                <p>End Date: {job.EndDate}</p>
-              </div>
-            ))}
-          </div>
-        </div>
-        <div className="col-3">
-          <div className="search-by-name">
-            <div>
-              <h4>Search By Name</h4>
-              {jobs.map((job) => (
-                <div key={job.id}>
-                  <input
-                    type="checkbox"
-                    checked={checkedState.includes(job.id)}
-                    onChange={(e) => handleFilterChecked(e, job.id)}
-                  />
-                  <span>{job.JobName}</span>
+          <div className="col-9">
+            <div className="job-data">
+              {filteredJobs.map((job) => (
+                <div key={job.id} className="job-card">
+                  <Link to={`/JobDetails/${job.id}`}>
+                    <img src={job.image} alt="Job" className="job-image" />
+                    <h2>{job.JobName}</h2>
+                  </Link>
+                  <p
+                    className={`job-description ${
+                      expandedJobId === job.id ? 'expanded' : ''
+                    }`}
+                    onClick={() => toggleJobDescription(job.id)}
+                  >
+                    {expandedJobId === job.id
+                      ? job.JobDescription
+                      : job.JobDescription.substring(0, 100) + '...'}
+                  </p>
+                  <p>Recruitment Goal: {job.RecuitmentGoal}</p>
+                  <p>Posted on: {job.PostDate}</p>
+                  <p>End Date: {job.EndDate}</p>
                 </div>
               ))}
             </div>
           </div>
+          <div className="col-3">
+            <div className="search-by-name">
+              <div>
+                <h4>Search By Name</h4>
+                {jobs.map((job) => (
+                  <div key={job.id}>
+                    <input
+                      type="checkbox"
+                      checked={checkedState.includes(job.id)}
+                      onChange={(e) => handleFilterChecked(e, job.id)}
+                    />
+                    <span>{job.JobName}</span>
+                  </div>
+                ))}
+              </div>
+            </div>
+          </div>
         </div>
       </div>
-    </div>
     </DefaultTemplate>
   );
 };
